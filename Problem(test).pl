a0b3ba8924c2d0eaa674a64664c2eb20ddc733dd
AshrafSamir <36134927+AshrafSamir@users.noreply.github.com>
2020-03-12 14:37:48 +0200

    not yet

--- Problem(test).pl
+++ Problem(test).pl
@@ -5,7 +5,7 @@ available_resources([[r1, 0], [r2, 0]]).
 allocated(p1, [r2]).
 allocated(p2, [r1]).
 allocated(p3, [r1]).
-allocated(p4, [r2,r1,r2,r2]).
+allocated(p4, [r2]).
 
 requested(p1, [r1]).
 requested(p3, [r2]).
@@ -19,7 +19,6 @@ safePath(T,S),!.
 safePath([H|T],[H|T1]):-
 safePath(T,T1).
 
-
 getRequesters([],_,[]).
 getRequesters([H1|T1],T2,R):-
 member(H1,T2),
@@ -27,12 +26,7 @@ getRequesters(T1,T2,R).
 getRequesters([H1|T1],T2,[H1|T3]):-
 getRequesters(T1,T2,T3),!.
 
-incAvail(P,R,N):-
-allocated(P,[R]),
-(available_resources([[R,S1],[_,_]]) *-> N is S1 + 1) ;
-(available_resources([[_,_],[R,S2]]) *-> N is S2 + 1) .
-
-getArr(S,R):-
+getArr(S,R,P):-
 processes(P),
 safePath(P,S),
 getRequesters(P,S,R).
@@ -43,6 +37,11 @@ findlen([X|Tail],Count):-
 findlen(Tail,Prev),
 Count is Prev + 1.
 
+delete(Element,[Element|Tail],Tail):-!.
+
+delete(Element,[Head|Tail],[Head|Tail1]) :-
+delete(Element,Tail,Tail1),!.
+
 
 remaining([],[]):-!.
 deAllocate([],[]):-!.
@@ -50,6 +49,13 @@ deAllocate([H|T], [H2|T1]):-
 allocated(H,[H2|T2]),
 remaining(T2,S),
 deAllocate(T,B1),!,
+append(S,B1,T1).
+
+reqForP([],[]):-!.
+reqForP([H|T], [H2|T1]):-
+requested(H,[H2|T2]),
+remaining(T2,S),
+reqForP(T,B1),!,
 append(S,B1,T1).
 
 remaining([H|T],[H|T1]):-
@@ -67,3 +73,31 @@ addAvailableR2(T1,0,T1):-!.
 addAvailableR2(T1,S1,[r2|T]):-
 Snew is S1 - 1,
 addAvailableR2(T1,Snew,T),!.
+
+getAllAvailRes(A):-
+getArr(S,R,P),
+deAllocate(S,New),
+getAvailResNum(N1,N2),
+addAvailableR1(New,N1,New1),
+addAvailableR2(New1,N2,A).
+
+getAllAvailRes(R,New):-
+getAllAvailRes(A),
+append(A,R,New).
+
+checkInAvail([],A):-!.
+checkInAvail([H|T],A):-
+member(H,A),
+checkInAvail(T,A),!.
+
+completeSafePath([],[],A):-!.
+completeSafePath([H|T],[H|T1],A):-
+reqForP([H],R),
+checkInAvail(R,A),
+getAllAvailRes(R,A),
+completeSafePath(T,T1,A),!.
+
+completed(F):-
+getArr(S,R,P),
+completeSafePath(R,New,A),
+append(S,New,F).
